4c859fe4918bcc2a50842b1ff20e055e932654f6
davidminsuchong <davidminsuchong@gmail.com>
2022-09-26 23:52:22 +0900

    2022/09/26

--- BAEKJOON[10989].py
+++ BAEKJOON[10989].py
@@ -1,12 +1,24 @@
 #10989
 
-import sys
-
 N = int(input())
 A = []
 B = [0]*10000
+C = ["0"]*N
 
 for i in range(N):
-    a = int(sys.stdin.readline())
+    a = input()
     A.append(a)
-    B[a-1] += 1
+    B[int(a)-1] += 1
+
+for i in range(9999):
+    B[i+1] += B[i]
+
+del B[9999]
+B = [0]+B
+
+for i in range(N):
+    C[B[int(A[i])-1]] = A[i]
+    B[int(A[i])-1]+=1
+
+for i in range(N):
+    print(C[i])
